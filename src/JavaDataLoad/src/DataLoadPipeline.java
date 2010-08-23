ca518ce381f9b021a3c5d24bce6b7d4d47705779
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-08-23 19:10:06 +0000

    init setup for dataload

---
+++ src/JavaDataLoad/src/DataLoadPipeline.java
@@ -0,0 +1,13 @@
+
+public class DataLoadPipeline {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to DataLoadPipeline");
+		System.out.println("This is just a test");
+
+	}
+
+}
