39f35bbdca48f0ad6d973fb9c87e0dfcd4527946
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-08-24 04:27:19 +0000

    added comments

--- trunk/src/JavaDataLoad/src/DB.java
+++ trunk/src/JavaDataLoad/src/DB.java
@@ -1,23 +1,42 @@
 import java.sql.*;
 
+/**
+ * @author Wei-Ting Lu
+ *
+ * This is a basic class that will allow Database coonection
+ * based on jtds and jdbc
+ */
 public class DB {
-    public DB() {}
+	
+	private Connection con; //The connection object
+	public DB() {
+		con = null;
+	}
 
-    public void dbConnect(String db_connect_string, 
-  String db_userid, String db_password)
-    {
-        try
-        {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(
-    db_connect_string, db_userid, db_password);
-            System.out.println("connected");
-            
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+	
+	/**
+	 * This method will connect to the database based on the given
+	 * connection string, userid, and password
+	 * 
+	 * @param db_connect_string the connection URL
+	 * @param db_userid the user id of the database
+	 * @param db_password the password of database
+	 */
+	public void dbConnect(String db_connect_string, String db_userid, String db_password)
+	{
+		try
+		{
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(
+					db_connect_string, db_userid, db_password);
+			con = conn;
+			System.out.println("connected");
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
 
