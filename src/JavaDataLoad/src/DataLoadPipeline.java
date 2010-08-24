0dd6336d181e8c1447322dabefb659447fa71d8b
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-08-24 18:26:11 +0000

    created insert data for dark matter

--- src/JavaDataLoad/src/DataLoadPipeline.java
+++ src/JavaDataLoad/src/DataLoadPipeline.java
@@ -11,10 +11,12 @@ public class DataLoadPipeline {
 		DB db = new DB();
 		Connection con = db.dbConnect("jdbc:jtds:sqlserver://fatboy.npl.washington.edu/NBODY", "NBODY-1", "TheWholeNchilada!");
 		//db.createTables(con);
-		db.createTablesDark(con, "wtltest_DarkJava");
-		db.createTablesGas(con, "wtltest_GasJava");
-		db.createTablesStar(con, "wtltest_StarJava");
-		db.createTablesMeta(con, "wtltest_metaJava");
+//		db.createTablesDark(con, "wtltest_DarkJava");
+//		db.createTablesGas(con, "wtltest_GasJava");
+//		db.createTablesStar(con, "wtltest_StarJava");
+//		db.createTablesMeta(con, "wtltest_metaJava");
+		db.insertDataDark(con, "wtlutest_DarkJava", 2097152, (float)1.07765e-07, (float)-0.477565, (float)-0.446872, 
+				(float)-0.45568, (float)0.100956, (float)0.057776, (float)0.0266779, (float)-0.113261, (float)9.6e-06);
 		db.dbClose(con);
 		System.out.println("Connected, but now exiting, goodbye.");
 	}
