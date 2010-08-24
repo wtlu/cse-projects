12eecee00b324596af4f105834141a5a7631d52a
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-08-24 19:18:17 +0000

    added insertDataStar, gas, dark, and meta

--- trunk/src/JavaDataLoad/src/DataLoadPipeline.java
+++ trunk/src/JavaDataLoad/src/DataLoadPipeline.java
@@ -17,6 +17,9 @@ public class DataLoadPipeline {
 //		db.createTablesMeta(con, "wtltest_metaJava");
 		db.insertDataDark(con, "wtltest_DarkJava", 2097152, (float)1.07765e-07, (float)-0.477565, (float)-0.446872, 
 				(float)-0.45568, (float)0.100956, (float)0.057776, (float)0.0266779, (float)-0.113261, (float)9.6e-06);
+		db.insertDataGas(con, "wtltest_GasJava", 50, 55, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+		db.insertDataStar(con, "wtltest_StarJava", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+		db.insertDataMeta(con, "wtltest_metaJava", 5, 333, 50, 50, 50, 50);
 		db.dbClose(con);
 		System.out.println("Connected, but now exiting, goodbye.");
 	}
