2b68b973a6c3ad23ca90487d2d47da71d571fad5
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-12 07:07:02 +0000

    finished stretch

--- src/CSE341/HW6/hw6.scm
+++ src/CSE341/HW6/hw6.scm
@@ -29,7 +29,22 @@
       lst
       (cons (+ (car lst) (cadr lst)) (collapse (cddr lst)))))
 
+; Pre: valid list
+; Post: takes list of int lst and doubles its length by replacing each
+; integer with two numbers that add up to the integer. The two numbers are
+; half of the original, but if original number is odd, then first number is higher
+; in absolute value than the second
+(define (stretch lst)
+  (if (null? lst)
+      ()
+      (let* ((first (car lst)) (half (quotient first 2))) 
+        (if (= 0 (modulo first 2))
+        (cons half (cons half (stretch (cdr lst))))
+        (cons (if (< half 0) (- half 1) (+ half 1)) (cons half (stretch (cdr lst))))))))
+
+
 ; Test values, delete when done
 (define test1 (list 1 2 3 4 5))
 (define test2 '(2 8 3.1 2.4 3 6.5))
 (define test3 '(13 4 27 9 48))
+(define test4 '(38 4 19 7 -5))
