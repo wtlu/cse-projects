aa2a17b6fbce435aa2defe95555a3a02a5273b83
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-12 10:02:58 +0000

    finished generate

--- trunk/src/CSE341/HW6/hw6.scm
+++ trunk/src/CSE341/HW6/hw6.scm
@@ -5,6 +5,7 @@
 ; Quiz Section: AA
 ; HW6 - This program contains several short individual functions
 
+; Pre: str is valid string and n is valid integer
 ; Post: returns the given string str repeated the given number of times n 
 (define (repl str n)
   (if (<= n 0)
@@ -20,7 +21,7 @@
       (list n)
       (cons 2 (list-twos (/ n 2)))))
 
-; Pre: valid list
+; Pre: valid list of numbers
 ; Post: takes list of numbers and collapse successive pairs of numbers
 ; by replacing the pair with the sum of the numbers. If list has odd length 
 ; then last value is not collapsed
@@ -29,7 +30,7 @@
       lst
       (cons (+ (car lst) (cadr lst)) (collapse (cddr lst)))))
 
-; Pre: valid list
+; Pre: valid int list
 ; Post: takes list of int lst and doubles its length by replacing each
 ; integer with two numbers that add up to the integer. The two numbers are
 ; half of the original, but if original number is odd, then first number is higher
@@ -42,7 +43,7 @@
         (cons half (cons half (stretch (cdr lst))))
         (cons (if (< half 0) (- half 1) (+ half 1)) (cons half (stretch (cdr lst))))))))
 
-; Pre: lst contains at least one element
+; Pre: lst is valid number list and contains at least one element
 ; Post: computes the sum of the squared values of all the numbers in the lst
 (define (sum-squares lst)
   (foldl + 0
@@ -57,6 +58,27 @@
   (if (null? lst1)
       ()
       (append (map (lambda (y) (list (car lst1) y))lst2) (all-pairs (cdr lst1) lst2))))
+
+; Pre: n is a valid integer and is greater than or equal to 0
+; Post: generates a random expression of depth n
+; expression of depth 0 would produce either sumbol x or sumbol y with equal probability
+; expression of depth greater than 0 it would produce one of the following five Scheme:
+; expressions, each with equal probability
+; (cos (* pi expr))
+; (sin (* pi expr))
+; (/ (+ expr expr) 2.0)
+; (/ (round (* expr 10.0)) 10.0)
+; (* expr expr)
+(define (generate n)
+  (let* ((random0or1 (round(random)))
+         (random0to4 (round(* (random) 4))))
+    (if (= n 0)
+    (if (= random0or1 0) 'x 'y)
+    (cond ((= random0to4 0) (list 'cos (list '* 'pi (generate (- n 1)))))
+          ((= random0to4 1) (list 'sin (list '* 'pi (generate (- n 1)))))
+          ((= random0to4 2) (list '/ (list '+ (generate (- n 1)) (generate (- n 1))) 2.0))
+          ((= random0to4 3) (list '/ (list 'round (list '* (generate (- n 1)) 10.0)) 10.0))
+          ((= random0to4 4) (list '* (generate (- n 1)) (generate (- n 1))))))))
 
 ; Test values, delete when done
 (define test1 (list 1 2 3 4 5))
