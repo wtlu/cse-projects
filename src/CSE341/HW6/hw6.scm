8f07a9c1b13835159fce12cbcc518c6a2080c5ff
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-12 06:40:54 +0000

    finished list-twos and collapse

--- src/CSE341/HW6/hw6.scm
+++ src/CSE341/HW6/hw6.scm
@@ -19,3 +19,17 @@
   (if (= 1 (modulo n 2))
       (list n)
       (cons 2 (list-twos (/ n 2)))))
+
+; Pre: valid list
+; Post: takes list of numbers and collapse successive pairs of numbers
+; by replacing the pair with the sum of the numbers. If list has odd length 
+; then last value is not collapsed
+(define (collapse lst)
+  (if (<= (length lst) 1)
+      lst
+      (cons (+ (car lst) (cadr lst)) (collapse (cddr lst)))))
+
+; Test values, delete when done
+(define test1 (list 1 2 3 4 5))
+(define test2 '(2 8 3.1 2.4 3 6.5))
+(define test3 '(13 4 27 9 48))
