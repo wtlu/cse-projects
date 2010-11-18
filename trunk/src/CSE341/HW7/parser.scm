bf63da98b0da13f2518d82d685d38c09571ca670
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-18 09:28:48 +0000

    added testing code

--- trunk/src/CSE341/HW7/parser.scm
+++ trunk/src/CSE341/HW7/parser.scm
@@ -103,8 +103,9 @@
 (define (parse-test lst)
   (let* ((expAns (parse-expression lst))
         (operator (cadr expAns))
+        (modedOp (if (symbol=? '<> operator) '(not equal?) operator))
         (expAns2 (parse-expression (cddr expAns)))
-        (code (list operator (car expAns) (car expAns2))))
+        (code (list modedOp (car expAns) (car expAns2))))
     (cons (eval code) (cdr expAns2))))
     
 
@@ -143,3 +144,69 @@
 (define test32 '(3.4 < 7.8 THEN 19))
 (define test33 '(2.3 - 4.7 ^ 2.4 <> SQR lparen 8 - 4.2 ^ 2 * 9 rparen FOO))
 (define test34 '(2 ^ 4 = 4 ^ 2))
+
+"parse-factor *****************"
+(parse-factor '(3.5 2.9 OTHER STUFF))
+'(3.5 2.9 OTHER STUFF)
+(parse-factor '(- 7.9 3.4 * 7.2))
+'(-7.9 3.4 * 7.2)
+(parse-factor '(lparen 7.3 - 3.4 rparen + 3.4))
+'(3.9 + 3.4)
+(parse-factor '(SQR lparen 12 + 3 * 6 - 5 rparen))
+'(5)
+(parse-factor '(- lparen 2 + 2 rparen * 4.5))
+'(-4 * 4.5)
+(parse-factor '(- 13 - 17 - 9))
+'(-13 - 17 - 9)
+
+"parse-element *****************"
+(parse-element '(2 ^ 2 ^ 3 THEN 450))
+'(64 THEN 450)
+(parse-element '(2 ^ 2 ^ -3 THEN 450))
+'(0.015625 THEN 450)
+(parse-element '(2.3 ^ 4.5 * 7.3))
+'(42.43998894277659 * 7.3)
+(parse-element '(7.4 + 2.3))
+'(7.4 + 2.3)
+(parse-term '(2 ^ 3 ^ 4 ^ 5 2 ^ 3 4 ^ 5))
+'(1152921504606846976 2 ^ 3 4 ^ 5)
+
+"parse-term *****************"
+(parse-term '(2.5 * 4 + 9.8))
+'(10.0 + 9.8)
+(parse-term '(38.7 / 2 / 3 THEN 210))
+'(6.45 THEN 210)
+(parse-term '(7.4 * lparen 2.4 - 3.8 rparen / 4 - 8.7))
+'(-2.59 - 8.7)
+(parse-term '(3 / 4 + 9.7))
+'(0.75 + 9.7)
+(parse-term '(2 * 3 * 4 + 3 * 8))
+'(24 + 3 * 8)
+(parse-term '(24 / 2 - 13.4))
+'(12.0 - 13.4)
+
+"parse-expression *****************"
+(parse-expression '(12.4 - 7.8 * 3.5 THEN 40))
+'(-14.9 THEN 40)
+(parse-expression '(2 + 3.4 - 7.9 <= 7.4))
+'(-2.5 <= 7.4)
+(parse-expression '(3 * 4 ^ 2 / 5 + SIN lparen 2 rparen))
+'(10.50929742682568)
+(parse-expression '(15 - 3 - 2 foo 2 + 2))
+'(10 foo 2 + 2)
+
+"parse-test *****************"
+(parse-test '(3.4 < 7.8 THEN 19))
+'(#t THEN 19)
+(parse-test '(2.3 - 4.7 ^ 2.4 <> SQR lparen 8 - 4.2 ^ 2 * 9 rparen FOO))
+'(#t FOO)
+(parse-test '(2 ^ 4 = 4 ^ 2))
+'(#t)
+
+"illegal *****************"
+;(parse-term '(2.5 * 4 *))
+;(parse-factor '(foo bar))
+;(parse-factor '(- foo))
+;(parse-term '(3 * foo))
+;(parse-factor '(lparen rparen))
+;(parse-factor '(lparen 2 + 3 4)) 
