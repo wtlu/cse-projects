1e76ec6e0bf8b80fa64c3e13e4ce5d94da230221
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-24 09:18:17 +0000

    now has a cache and fakes LET and PRINT. currently only recognizes them, not executing them

--- src/CSE341/HW8/interpreter.scm
+++ src/CSE341/HW8/interpreter.scm
@@ -8,6 +8,11 @@
 (include "hw7.scm")
 (include "hw8-support.scm")
 
+; cache of declared variables, initially empty
+(define cache null)
+
+; Pre:
+; Post: runs the given BASIC code that is passed in as a lst
 (define (run-program lst)
   (cond [(null? lst) ()]
         [(null? (car lst)) ()]
@@ -16,5 +21,8 @@
                (rest (cdr lst))
                ;(debug2 (display rest))
                )
-           (cond [(symbol=? 'END (caadr firstLine)) (display '"PROGRAM TERMINATED")]
-                 [(symbol=? 'REM (caadr firstLine)) (run-program rest)]))]))
+           (cond [(symbol=? 'END (caadr firstLine)) (display "PROGRAM TERMINATED")]
+                 [(symbol=? 'REM (caadr firstLine)) (begin (display "comment")(newline)(run-program rest))]
+                 [(symbol=? 'LET (caadr firstLine)) (begin (display "let ") (newline) (run-program rest))]
+                 [(symbol=? 'PRINT (caadr firstLine)) (let* ()
+                                                        (begin (display "print statement")(newline)(run-program rest)))]))]))
