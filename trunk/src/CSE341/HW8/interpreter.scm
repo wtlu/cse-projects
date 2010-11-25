90cb412122e4fd8f7622a9431b1c70ced5cdfb8a
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-11-25 00:56:19 +0000

    got rid of debug comments

--- trunk/src/CSE341/HW8/interpreter.scm
+++ trunk/src/CSE341/HW8/interpreter.scm
@@ -18,12 +18,12 @@
         [(null? (car lst)) ()]
         [(let* ((firstLine (car lst))
                (rest (cdr lst)))
-           ;(display firstLine)
-           ;(display rest)
            (cond [(symbol=? 'END (caadr firstLine)) (display "PROGRAM TERMINATED")]
-                 [(symbol=? 'REM (caadr firstLine)) (begin (display "comment")(newline)(run-program rest))]
-                 [(symbol=? 'LET (caadr firstLine)) (begin (display "let ") (newline) (process-let (cdadr firstLine) (car firstLine))(run-program rest))]
-                 [(symbol=? 'PRINT (caadr firstLine)) (begin (display "print statement")(newline)(process-print (cdadr firstLine) (car firstLine)) (run-program rest))]))]))
+                 [(symbol=? 'REM (caadr firstLine)) (run-program rest)]
+                 [(symbol=? 'LET (caadr firstLine)) 
+                  (begin (process-let (cdadr firstLine) (car firstLine))(run-program rest))]
+                 [(symbol=? 'PRINT (caadr firstLine)) 
+                  (begin (process-print (cdadr firstLine) (car firstLine)) (run-program rest))]))]))
 
 ; Pre: statement is a let statement with correct line number
 ; Post: process the let statement
@@ -31,37 +31,22 @@
   (let* ((varName(car lst))
          (equalsSign(cadr lst))
          (restLine(cddr lst)))
-    ;(display "The Whole line is")
-    ;(display lst)
-    (display "\nThe rest of the line is ")
-    (display restLine) (newline)
   (cond [(and (variable? varName) (symbol=? '= equalsSign))
          (let* ((resultLst(subsitude-expression restLine n))
                 (resultLst2(try-parse-expression resultLst n)))
-           (display "the resulting lst is")
-           (display resultLst2)
-           (display "processing let statement") (newline) (set! symbolTable (cons (cons varName (car(try-parse-expression (subsitude-expression restLine n) n))) symbolTable))
-           (display symbolTable))]
+            (set! symbolTable (cons (cons varName (car(try-parse-expression (subsitude-expression restLine n) n))) symbolTable)))]
         [(display "ILLEGAL LET")])))
 
 ; Pre: statement is a valid expression
 ; Post: process the list and replace any variables found with the one from symbolTable
 (define (subsitude-expression lst n)
-  ;(display "Subbing in this list:")
-  ;(display lst)
-  ;(newline)
   (cond [(null? lst) lst]
         [(let ((first (car lst))
                (rest (cdr lst)))
-           (cond [(variable? first) (display "looking up:")(display first)
-                                    (let ((lookup (assoc first symbolTable)))
-                                      (display "lookup is:")
-                                      (display lookup)
+           (cond [(variable? first) (let ((lookup (assoc first symbolTable)))
                                       (if lookup (cons (cdr lookup) (subsitude-expression rest n))
                                           (error "LINE" n': "DEBUG ILLEGAL EXPRESSION")))]
                  [else (cons first (subsitude-expression rest n))]))])
-  ;(display "result lst is:")
-  ;(display lst) (newline)
   )
 
 ; Pre: statement is a print statement with correct line number
