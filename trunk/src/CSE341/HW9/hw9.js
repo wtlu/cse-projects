7d12c3dbad92ca2a74ea1c60b67751b3aad01b8e
weiting.t.lu <weiting.t.lu@86713b35-544e-2226-0422-2cabe6323f95>
2010-12-05 05:42:37 +0000

    initialized hw9 with files

---
+++ trunk/src/CSE341/HW9/hw9.js
@@ -0,0 +1,44 @@
+/* CSE341
+Wei-Ting Lu 
+12/5/2010
+TA: Robert Johnson 
+Quiz Section: AA
+HW9 - This program contains several short individual functions
+*/
+
+
+// Pre: string is valid, n passed in is valid integer
+// Post: returns the string repeated n times. If n is <= 0, returns empty string
+// 			if n is not passed, assume an n of 1
+function repl() {
+	//Todo
+    print("work in progress");
+}
+
+// Pre: integer is valid
+// Post: returns true if the integer is a prime number, false otherwise
+function isPrime() {
+	//Todo
+    print("work in progress");
+}
+
+// Pre: object that stores various attributes of box as its properties
+// is valid. 
+// Post: draws a text fiture of a rectangular box based on the passed in object with
+// following properties. If property is absent, use default values shown
+// width: number of characters wide for the box (default 10)
+// height: number of characters wide for the box (default 5)
+// border: character to use to draw around the outside of the box (default "*")
+// filling: character to use to draw in the interior of the box (default ".")
+function box() {
+	//Todo
+    print("work in progress");
+}
+
+// Pre: string is valid
+// Post: returns a new string with same characters as original, but
+// with the text converted to "Pig Latin"
+function pigLatin() {
+	//Todo
+    print("work in progress");
+}
